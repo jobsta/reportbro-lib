9f3dcc9a024de7248fa41de64adf79767d8f8d62
Alex Hartmann <23240822+alhman@users.noreply.github.com>
2019-11-08 15:00:01 +0100

    show error details when returning string representation of ReportBro exception

--- reportbro/errors.py
+++ reportbro/errors.py
@@ -4,6 +4,10 @@ class ReportBroError(Exception):
     def __init__(self, error):
         self.error = error
 
+    def __str__(self):
+        return 'ReportBroError: ' + ', '.join(
+            ['{key}={value}'.format(key=key, value=self.error.get(key)) for key in self.error])
+
 
 class Error(dict):
     def __init__(self, msg_key, object_id=None, field=None, info=None, context=None):
