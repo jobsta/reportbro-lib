dbd6cac18ba60e2a1d3b6dd7c62b205a92e2de0a
Bernhard Miklautz <bmiklautz+jobsta@shacknet.at>
2024-03-25 16:26:52 +0100

    Fix deprecation warning in test_resources
    Deprecated in python 3.11. Warning was:
    
    ```
    DeprecationWarning: path is deprecated. Use files() instead.
    ```

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,7 +1,14 @@
 import importlib.resources
 
-
 def test_resources_waterpark_present():
-	with importlib.resources.path('reportbro.data', 'logo_watermark.png') as p:
-		watermark_filename = p
-		assert(watermark_filename.exists())
+    WATERMARK='logo_watermark.png'
+    PACKAGE='reportbro.data'
+
+    if hasattr(importlib.resources, "as_file"):  # Python 3.9+
+        from importlib.resources import as_file, files
+        with as_file(files(PACKAGE).joinpath(WATERMARK)) as p:
+            assert(p.exists())
+    else:
+        with importlib.resources.path(PACKAGE, WATERMARK) as p:
+            watermark_filename = p
+            assert(watermark_filename.exists())
